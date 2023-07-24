a5200d3ea2626b03707270cb438e666a8fd1628c
Ars Vladimirov <grokhael@gmail.com>
2023-07-24 17:52:23 +0600

    гзв сгыещь вфеф

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,9 +1,7 @@
-**Photometric stereo training data set with annotated leaf masks** is a dataset for instance segmentation tasks. It is used in the agriculture industry.
+**Photometric stereo training data set with annotated leaf masks** is a dataset for instance segmentation, semantic segmentation, and object detection tasks. It is used in the biological and genetic researches. 
 
-The dataset consists of 221 images with 2433 labeled objects belonging to 1 single class (_leaf_).
+The dataset consists of 221 images with 2433 labeled objects belonging to 1 single class (*leaf*).
 
-Each image in the Photometric stereo leafs dataset has pixel-level instance segmentation annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into semantic segmentation (only one mask for every class) or object detection (bounding boxes for every object) tasks. All images are labeled (i.e. with annotations). There is 1 split in the dataset: _ds_ (221 images). The dataset was released in 2019.
+Images in the Photometric stereo leafs dataset have pixel-level instance segmentation annotations. Due to the nature of the instance segmentation task, it can be automatically transformed into a semantic segmentation (only one mask for every class) or object detection (bounding boxes for every object) tasks. All images are labeled (i.e. with annotations). There are no pre-defined <i>train/val/test</i> splits in the dataset. The dataset was released in 2019 by the [University of Edinburgh, Scotland](https://www.ed.ac.uk/).
 
-Here are the visualized examples for each of the 1 classes:
-
-[Dataset classes](https://github.com/dataset-ninja/photometric-stereo-leafs/raw/main/visualizations/horizontal_grid.webm)
+<img src="https://github.com/dataset-ninja/photometric-stereo-leafs/raw/main/visualizations/poster.png">
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,11 +20,20 @@ PROJECT_NAME_FULL: Optional[str] = "Photometric stereo training data set with an
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-INDUSTRIES: List[Industry] = [Industry.Agriculture()]
-CV_TASKS: List[CVTask] = [CVTask.InstanceSegmentation()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Research.Biological(), Research.Genetic()]
+CATEGORY: Category = Category.Biology()
+
+CV_TASKS: List[CVTask] = [
+    CVTask.InstanceSegmentation(),
+    CVTask.SemanticSegmentation(),
+    CVTask.ObjectDetection(),
+]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
-RELEASE_YEAR: int = 2019
+RELEASE_DATE: Optional[str] = "2019-03-25"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://datashare.ed.ac.uk/handle/10283/3280"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -37,10 +54,14 @@ DOWNLOAD_ORIGINAL_URL: Optional[
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
-PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = None
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+PAPER: Optional[str] = "https://academic.oup.com/gigascience/article/8/5/giz056/5498634?login=false"
+CITATION_URL: Optional[str] = "https://datashare.ed.ac.uk/handle/10283/3280"
+AUTHORS: Optional[List[str]] = ["Scorza, Livia", "Bernotas, Gytis", "McCormick, Alistair"]
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "University of Edinburgh, Scotland"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://www.ed.ac.uk/"
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -55,10 +76,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -70,13 +96,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
+    settings["release_date"] = RELEASE_DATE
     settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
